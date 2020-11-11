9c176eb68f734fc1b4e7076cb65ae3deffe29ae1
Sandeep <71704157+sandeepkchandran@users.noreply.github.com>
2020-11-11 15:17:33 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,12 @@ async function run() {
   try {
     const path = core.getInput('path');
 
-    core.debug(`Load package.json at ${path}`);
+    core.debug(`Load adobe.json at ${path}`);
 
     const result = getPackageVersion(path);
 
-    core.debug(`set output: version: ${result}`);
-    core.setOutput('version', result);
+    core.debug(`set output: name: ${result}`);
+    core.setOutput('name', result);
   } catch (error) {
     core.setFailed(error.message);
   }
